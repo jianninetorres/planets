85974cbddc3dd77b913d01d1c1b03f9a67bd1c86
Jiannine Torres <jianninetorres@gmail.com>
2022-08-13 16:39:22 -0400

    Create a helper file starting with Planet data type

---
+++ components/nav/helpers.ts
@@ -0,0 +1,26 @@
+export type PlanetType = {
+  name: string;
+  themeColour: string;
+  path: string;
+  overview: {
+    content: string;
+    source: string;
+  };
+  structure: {
+    content: string;
+    source: string;
+  };
+  geology: {
+    content: string;
+    source: string;
+  };
+  rotation: string;
+  revolution: string;
+  radius: string;
+  temperature: string;
+  images: {
+    planet: string;
+    internal: string;
+    geology: string;
+  };
+};
