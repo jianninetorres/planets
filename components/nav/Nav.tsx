55e4b771077501ee922dcb30074de49bdf6734cf
Jiannine Torres <jianninetorres@gmail.com>
2022-08-13 01:28:53 -0400

    Improve logic for responsive nav

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
-import Image from "next/image";
-import data from "../../data/data.json";
+import { useEffect, useState } from "react";
 import FutureImage from "next/future/image";
+
 import useStyles from "./styles";
 import menuIcon from "../../public/menu-icon.svg";
 import chevronIcon from "../../public/chevron-right.svg";
+import data from "../../data/data.json";
 
 const Nav = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [isWide, setIsWide] = useState(false);
   const { classes } = useStyles();
+  const hasWindow = typeof window !== "undefined";
+
+  // immediately set isWide on load so that the nav will be visible on wider viewports
+  useEffect(() => {
+    setIsWide(window.innerWidth > 767);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 767) {
+        setIsWide(true);
+      } else {
+        setIsWide(false);
+      }
+    });
+  }, [hasWindow]);
 
   const planets = data.map((planet) => (
     <li key={planet.name} className={classes.planets}>
@@ -46,7 +63,12 @@ const Nav = () => {
           className={classes.menuIcon}
         />
       </div>
-      <div className={isVisible ? classes.isVisible : classes.isNotVisible}>
+
+      <div
+        className={
+          isVisible || isWide ? classes.isVisible : classes.isNotVisible
+        }
+      >
         <ul className={classes.planetsContainer}>{planets}</ul>
       </div>
     </nav>
