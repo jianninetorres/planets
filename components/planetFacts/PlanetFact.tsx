f2b4e66830763a4cd099e1063e2ca1ff17fe9dab
Jiannine Torres <jianninetorres@gmail.com>
2022-08-18 13:54:06 -0400

    Improve styles for PlanetFact component

--- components/planetFacts/PlanetFact.tsx
+++ components/planetFacts/PlanetFact.tsx
@@ -1,13 +1,36 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    border: `1px solid ${theme.colors.lightgrey}`,
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+  },
+  title: {
+    fontSize: "0.65em",
+    color: theme.colors.lightgrey,
+    letterSpacing: "0.73px",
+    margin: 0,
+  },
+  data: {
+    fontSize: "20px",
+    margin: 0,
+  },
+}));
+
 interface PlanetFactProps {
   title: string;
   data: string;
 }
 
 const PlanetFact = (props: PlanetFactProps) => {
+  const { classes } = useStyles();
   return (
-    <div>
-      <h4>{props.title.toUpperCase()}</h4>
-      <h2>{props.data}</h2>
+    <div className={classes.container}>
+      <h4 className={classes.title}>{props.title.toUpperCase()}</h4>
+      <h2 className={classes.data}>{props.data}</h2>
     </div>
   );
 };
--- components/planetFacts/PlanetFactsContainer.tsx
+++ components/planetFacts/PlanetFactsContainer.tsx
@@ -1,5 +1,13 @@
 import { PlanetType } from "../nav/helpers";
 import PlanetFact from "./PlanetFact";
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params) => ({
+  container: {
+    display: "grid",
+    gridGap: "8px",
+  },
+}));
 
 interface PlanetFactsContainerProps {
   data: {
@@ -11,13 +19,14 @@ interface PlanetFactsContainerProps {
 }
 
 const PlanetFactsContainer = (props: PlanetFactsContainerProps) => {
+  const { classes } = useStyles();
   return (
-    <>
+    <div className={classes.container}>
       <PlanetFact title="Rotation Time" data={props.data.rotationTime} />
       <PlanetFact title="Revolution Time" data={props.data.revolutionTime} />
       <PlanetFact title="Radius" data={props.data.radius} />
       <PlanetFact title="Average Temp." data={props.data.avgTemp} />
-    </>
+    </div>
   );
 };
 
