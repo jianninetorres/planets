d81bde360e3292d2ed76a39789e575b954ec9921
Jiannine Torres <jianninetorres@gmail.com>
2022-08-17 23:39:20 -0400

    Start content setup

--- pages/[planet].tsx
+++ pages/[planet].tsx
@@ -1,13 +1,37 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import data from "../data/data.json";
+import { useRouter } from "next/router";
 
 const Planet: NextPage = () => {
+  const router = useRouter();
+  const name = router.query.planet;
+  const content = data.find((planet) => {
+    return planet.path === `/${name}`;
+  });
+
+  if (!content) {
+    return (
+      <>
+        <Head>
+          <title>Out of this world</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <div>It looks like you’ve left our solar system :(</div>
+      </>
+    );
+  }
+
   return (
     <div className="">
       <Head>
-        <title>Home</title>
+        <title>{content.name === "Earth" ? "Home" : content.name}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <div>{content.name}</div>
     </div>
   );
 };
