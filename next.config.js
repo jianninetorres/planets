6d08adc3dae937a14ac7bd5acbba1fd8621abb07
Jiannine Torres <jianninetorres@gmail.com>
2022-08-17 23:35:25 -0400

    Set Home to Earth
    This makes it easier to find the data based on the router query in pages/[planet].tsx

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/earth",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
