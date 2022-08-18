1b4765c76c07550a6d59831d63c1c49d3e6e8c70
Jiannine Torres <jianninetorres@gmail.com>
2022-08-18 11:03:47 -0400

    Add custom page styles

--- pages/[planet].tsx
+++ pages/[planet].tsx
@@ -2,9 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import data from "../data/data.json";
 import { useRouter } from "next/router";
+import useStyles from "./styles";
+
 import PlanetImage from "../components/planetImage/PlanetImage";
 
 const Planet: NextPage = () => {
+  const { classes } = useStyles();
   const router = useRouter();
   const name = router.query.planet;
   const content = data.find((planet) => {
@@ -27,7 +30,7 @@ const Planet: NextPage = () => {
   }
 
   return (
-    <div className="">
+    <div className={classes.container}>
       <Head>
         <title>{content.name === "Earth" ? "Home" : content.name}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
---
+++ pages/styles.tsx
@@ -0,0 +1,9 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params) => ({
+  container: {
+    padding: theme.spacing.md,
+  },
+}));
+
+export default useStyles;
